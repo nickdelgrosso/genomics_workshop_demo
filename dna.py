c8326fb65e462313840b212685e8a71bc9d43523
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-09-25 15:07:56 +0200

    first commit.  added DNA class from workshop.

---
+++ dna.py
@@ -0,0 +1,37 @@
+complimentary_nucleotides = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+
+
+class DNA:
+    def __init__(self, sequence: str):
+        self.sequence = sequence
+        if not self._check_validity():
+            raise ValueError("Bad sequence. Sequences must only contain G, C, A, and T")
+
+    def __eq__(self, other):
+        return True if str(self) == str(other) else False
+
+    def __str__(self):
+        return self.sequence
+
+    def __repr__(self):
+        return "DNA(sequence='{}')".format(self.sequence)
+
+    def _check_validity(self):
+        are_good = (nucleotide.upper() in 'GCAT' for nucleotide in self.sequence)
+        return True if all(are_good) else False
+
+    @property
+    def complimentary_sequence(self):
+        return DNA(''.join(complimentary_nucleotides[nt.upper()] for nt in self.sequence))
+
+
+if __name__ == '__main__':
+    try:
+        assert DNA('ATB')
+    except ValueError:
+        pass
+    assert DNA('GTC').complimentary_sequence == DNA('CAG')
+    assert DNA('ATC').complimentary_sequence == DNA('TAG')
+    assert DNA('ATC').complimentary_sequence == 'TAG'
+
+    print('it worked!')
