274707707eed1b566507f99244e39732731ed480
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-09-25 16:26:45 +0200

    Added RNA

---
+++ rna.py
@@ -0,0 +1,26 @@
+complimentary_nucleotides = {'A': 'U', 'U': 'A', 'C': 'G', 'G': 'C'}
+
+
+class RNA:
+    def __init__(self, sequence: str):
+        self.sequence = sequence
+        if not self._check_validity():
+            raise ValueError("Bad sequence. Sequences must only contain G, C, A, and U")
+
+    def __eq__(self, other):
+        return True if str(self) == str(other) else False
+
+    def __str__(self):
+        return self.sequence
+
+    def __repr__(self):
+        return "RNA(sequence='{}')".format(self.sequence)
+
+    def _check_validity(self):
+        are_good = (nucleotide.upper() in 'GCAU' for nucleotide in self.sequence)
+        return True if all(are_good) else False
+
+    @property
+    def complimentary_sequence(self):
+        return RNA(''.join(complimentary_nucleotides[nt.upper()] for nt in self.sequence))
+
---
+++ test_rna.py
@@ -0,0 +1,10 @@
+from rna import RNA
+import pytest
+
+def test_bad_sequence_raises_error():
+    with pytest.raises(ValueError):
+        RNA('ATB')
+
+def test_complimentary_sequence_works():
+    assert RNA('GUC').complimentary_sequence == RNA('CAG')
+    assert RNA('AUC').complimentary_sequence == RNA('UAG')
