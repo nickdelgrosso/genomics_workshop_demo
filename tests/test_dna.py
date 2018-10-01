636ec4ceb3a5ea13384f625b2a5056666f403b3f
Lisa Fischer <lancino@web.de>
2018-10-01 13:48:42 +0200

    GC content 50%

--- tests/test_dna.py
+++ tests/test_dna.py
@@ -8,3 +8,13 @@ def test_bad_sequence_raises_error():
 def test_complimentary_sequence_works():
     assert DNA('GTC').complimentary_sequence == DNA('CAG')
     assert DNA('ATC').complimentary_sequence == DNA('TAG')
+
+
+
+#def test_gc_content_sequence_works():
+ #   assert DNA('GC').gc_content > 0.5
+def test_is_gc_rich():
+    assert DNA('GTGT').gc_content() == 0.5
+ #   length = len(sequence)
+  #  c_count = sequence.upper().count('C')
+   # g_count = sequence.upp
