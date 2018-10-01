a712723d8129c292838c0e480b43c797b7d4f7c4
Drago Guggiana <drguggiana@gmail.com>
2018-10-01 13:48:51 +0200

    added gc_content property

--- tests/test_dna.py
+++ tests/test_dna.py
@@ -8,3 +8,8 @@ def test_bad_sequence_raises_error():
 def test_complimentary_sequence_works():
     assert DNA('GTC').complimentary_sequence == DNA('CAG')
     assert DNA('ATC').complimentary_sequence == DNA('TAG')
+
+def test_gc_content():
+    assert DNA('ATTTATGGCC').gc_content == 0.4
+    assert DNA('AGGTATGGCC').gc_content == 0.6
+    assert DNA('ATAT').gc_content == 0
