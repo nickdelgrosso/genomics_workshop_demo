51df614e5f1a76869872d3f89fdea74d4ff76d80
Sonja Blumenstock <blumenstock@neuro.mpg.de>
2018-10-01 13:49:43 +0200

    added check_polyA function to the class RNA

--- tests/test_rna.py
+++ tests/test_rna.py
@@ -8,3 +8,7 @@ def test_bad_sequence_raises_error():
 def test_complimentary_sequence_works():
     assert RNA('GUC').complimentary_sequence == RNA('CAG')
     assert RNA('AUC').complimentary_sequence == RNA('UAG')
+
+
+def test_check_polyA():
+    assert RNA('A'*50)._check_polyA() == 'Sequence contains a poly A tail of 50 adenines'
